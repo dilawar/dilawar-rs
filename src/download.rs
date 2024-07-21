635f7245f4110a30105e179c96e3e9c99bdd45bd
Dilawar Singh <dilawar@subcom.tech>
2024-07-21 11:40:59 +0530

    feat: Function to download a URL to a file

---
+++ src/download.rs
@@ -0,0 +1,42 @@
+//! Download module.
+
+use std::io::Write;
+use std::path::Path;
+
+/// Download a file to a given file.
+pub fn download_url<U: reqwest::IntoUrl + std::fmt::Display>(
+    url: U,
+    output: &Path,
+    sha256: Option<[u8; 32]>,
+) -> anyhow::Result<usize> {
+    use sha2::Digest;
+
+    tracing::info!("Downloading {url} to {output:?}...");
+
+    let client = reqwest::blocking::Client::new();
+
+    // This will store the file in memory, shouldn't be a real concern since
+    // it is only 15-20MB
+    let bytes = client
+        .get(url)
+        // The request gets rejected with no user-agent
+        .header("User-Agent", "curl/8.1.2")
+        .send()?
+        .bytes()?;
+
+    if let Some(sha256) = sha256 {
+        let mut hasher = sha2::Sha256::new();
+        hasher.update(&bytes);
+        let hash: [u8; 32] = hasher.finalize().into();
+
+        if sha256 != hash {
+            anyhow::bail!("SHA256 mismatch!");
+        }
+    }
+
+    let mut file = std::fs::File::create(output)?;
+    file.write_all(&bytes)?;
+    file.flush()?;
+
+    Ok(bytes.len())
+}
